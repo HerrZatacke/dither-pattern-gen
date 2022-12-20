5b60a7432f625bf70537ba151c6a53d1b53ea669
Andreas Hahn <zatacke@gmail.com>
2022-12-20 23:24:52 +0100

    lints

--- src/generatePattern.js
+++ src/generatePattern.js
@@ -5,13 +5,13 @@ const generatePattern = ({
   const pattern = [];
 
   for (let x = 0; x < 4; x += 1) {
-    xDim = [];
+    const xDim = [];
     pattern.push(xDim);
     for (let y = 0; y < 4; y += 1) {
-      yDim = [];
+      const yDim = [];
       xDim.push(yDim);
       for (let z = 0; z < 3; z += 1) {
-        yDim.push(baseValues[z][orderPattern[x * 4 + y]]);
+        yDim.push(baseValues[z][orderPattern[(x * 4) + y]]);
       }
     }
   }
