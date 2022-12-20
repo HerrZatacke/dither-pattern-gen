2d613343fb1610d3667287739ab34a97cb346e39
Andreas Hahn <zatacke@gmail.com>
2022-12-21 00:56:40 +0100

    simplify build process

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,7 +1,4 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
-const CopyPlugin = require('copy-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const common = require('./webpack.common');
 
 module.exports = merge(common(), {
@@ -9,22 +6,7 @@ module.exports = merge(common(), {
   devtool: false,
   stats: 'errors-warnings',
   performance: {
-    maxEntrypointSize: 300000,
-    maxAssetSize: 300000,
+    maxEntrypointSize: 500000,
+    maxAssetSize: 500000,
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.join(process.cwd(), 'src', 'assets', 'images'),
-          to: path.join(process.cwd(), 'dist', 'images'),
-        },
-      ],
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      reportFilename: 'bundles.html',
-      openAnalyzer: false,
-    }),
-  ],
 });
